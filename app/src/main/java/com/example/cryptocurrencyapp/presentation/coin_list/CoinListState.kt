e71de939d1c21f38bbba47ffc80448c341986fb3
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-03-21 17:16:04 +0530

    Crypto

---
+++ app/src/main/java/com/example/cryptocurrencyapp/presentation/coin_list/CoinListState.kt
@@ -0,0 +1,10 @@
+package com.example.cryptocurrencyapp.presentation.coin_list
+
+import com.example.cryptocurrencyapp.common.Resource
+import com.example.cryptocurrencyapp.domain.model.Coin
+
+data class CoinListState(
+    val isLoading: Boolean = false,
+    val coin: List<Coin> = emptyList(),
+    val error: String = ""
+)
